4f68e97ae3c67fb14bb4f956f79747a8e9ed6fb3
Bruno Santos <brunocobal@hotmail.com>
2021-06-30 19:09:01 -0300

    System Done

--- src/main/java/com/master/product/Main.java
+++ src/main/java/com/master/product/Main.java
@@ -6,60 +6,59 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static List<Product> products = new ArrayList<>();
+	public static List<Product> products = new ArrayList<>();
 
-    public static void main(String[] args) {
-        System.out.println("Welcome to the manager system!!!\n     ----");
-        while (true) {
-            System.out.println("1 - Register your products\n2 - List products name\n3 - Detailed List\n0 - Exit");
+	public static void main(String[] args) {
+		System.out.println("Welcome to the manager system!!!\n     ----");
+		while (true) {
+			System.out.println("1 - Register your products\n2 - List products name\n3 - Detailed List\n0 - Exit");
 
-            Scanner s = new Scanner(System.in);
+			Scanner sc = new Scanner(System.in);
 
-            String op = s.next();
-            switch (op) {
-                case "1":
-                    products.add(register());
-                    break;
-                case "2":
-                    products.forEach(e -> System.out.println(e.getName()));
-                    break;
-                case "3":
-                	products.forEach(e -> System.out.println("-Name: " + e.getName() + "-Price: " + e.getPrice()
-                	 + "-Quantity: " + e.getQuantity() + "-Description: " + e.getDescription()));
-                    break;
-                case "0":
-                    System.exit(0);
-                    break;
-                default:
-                    System.out.println("This option doesn't exist");
-                    break;
-            }
-        }
-    }
+			String op = sc.next();
+			switch (op) {
+			case "1":
+				products.add(register());
+				break;
+			case "2":
+				products.forEach(e -> System.out.println(e.getName()));
+				break;
+			case "3":
+				products.forEach(e -> System.out.println("-Name: " + e.getName() + "-Price: " + e.getPrice()
+						+ "-Quantity: " + e.getQuantity() + "-Description: " + e.getDescription()));
+				break;
+			case "0":
+				System.out.println("Thanks for using our services!!!");
+				System.exit(0);
+				break;
+			default:
+				System.out.println("This option doesn't exist");
+				break;
+			}
+		}
+	}
 
-    public static void dataValidation(Double price) {
-        if (!(price instanceof Double)) {
-            throw new InvalidNumberException();
-        }
-    }
+	public static void dataValidation(Double price) {
+		if (!(price instanceof Double)) {
+			throw new InvalidNumberException();
+		}
+	}
 
-    public static Product register() {
-        Scanner s = new Scanner(System.in);
-        System.out.print("Name: ");
-        String name = s.next();
-        System.out.print("Price: ");
-        Double price = s.nextDouble();
-        System.out.print("Description: ");
-        String description = s.next();
-        System.out.print("Quantity: ");
-        Integer quantity = s.nextInt();
-        System.out.print("Category: ");
-        String category = s.next();
+	public static Product register() {
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Name: ");
+		String name = scan.next();
+		System.out.print("Price: ");
+		Double price = scan.nextDouble();
+		System.out.print("Description: ");
+		String description = scan.next();
+		System.out.print("Quantity: ");
+		Integer quantity = scan.nextInt();
 
-        Product product = new Product(name, price, description, quantity);
-        
-        return product;
-      
-    }
+		Product product = new Product(name, price, description, quantity);
+
+		return product;
+
+	}
 
 }
--- src/main/java/com/master/product/Product.java
+++ src/main/java/com/master/product/Product.java
@@ -2,55 +2,52 @@ package com.master.product;
 
 public class Product {
 
-    private String name;
-    private double price;
-    private String description;
-    private int quantity;
+	private String name;
+	private double price;
+	private String description;
+	private int quantity;
 
-    public Product() {
-        super();
-    }
+	public Product() {
+		super();
+	}
 
-    public Product(String name, double price, String description, int quantity) {
-        super();
-        this.name = name;
-        this.price = price;
-        this.description = description;
-        this.quantity = quantity;
-    }
+	public Product(String name, double price, String description, int quantity) {
+		super();
+		this.name = name;
+		this.price = price;
+		this.description = description;
+		this.quantity = quantity;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public double getPrice() {
-        return price;
-    }
+	public double getPrice() {
+		return price;
+	}
 
-    public void setPrice(double price) {
-        this.price = price;
-    }
+	public void setPrice(double price) {
+		this.price = price;
+	}
 
-    public String getDescription() {
-        return description;
-    }
+	public String getDescription() {
+		return description;
+	}
 
-    public void setDescription(String description) {
-        this.description = description;
-    }
+	public void setDescription(String description) {
+		this.description = description;
+	}
 
-    public int getQuantity() {
-        return quantity;
-    }
+	public int getQuantity() {
+		return quantity;
+	}
 
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
-    }
-
-
-
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
 }
